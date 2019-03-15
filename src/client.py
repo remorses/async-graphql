87125d087a3a3b1170e2a410ff7295463fae9b9e
remorses <beats.by.morse@gmail.com>
2019-03-15 19:01:47 +0100

    init

---
+++ src/client.py
@@ -0,0 +1,36 @@
+import urllib.request
+import json
+import asyncio
+
+class Client:
+    def __init__(self, endpoint, loop=None):
+        self.endpoint = endpoint
+        self.token = None
+        self.headername = None
+        self.loop = loop or asyncio.get_event_loop()
+
+    async def execute(self, query, variables=None):
+        return self.loop.run_in_executor(None, lambda: self._send(query, variables))
+
+    def inject_token(self, token, headername='Authorization'):
+        self.token = token
+        self.headername = headername
+
+    def _send(self, query, variables):
+        data = {'query': query,
+                'variables': variables}
+        headers = {'Accept': 'application/json',
+                   'Content-Type': 'application/json'}
+
+        if self.token is not None:
+            headers[self.headername] = '{}'.format(self.token)
+
+        req = urllib.request.Request(self.endpoint, json.dumps(data).encode('utf-8'), headers)
+
+        try:
+            response = urllib.request.urlopen(req)
+            return response.read().decode('utf-8')
+        except urllib.error.HTTPError as e:
+            # print((e.read()))
+            # print('')
+            raise e
